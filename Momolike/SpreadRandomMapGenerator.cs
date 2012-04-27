ba00b3e4a8d1a73f0e7ce4c5114718c9e76df898
Gabriel Wilds <jetsetlemming@gmail.com>
2012-04-27 15:11:41 -0400

    Added XNA project, and added new map generation type

---
+++ Momolike/SpreadRandomMapGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapGen
+{
+    public class SpreadRandomMapGenerator : MapGenerator
+    {
+        public SpreadRandomMapGenerator()
+        {
+
+        }
+
+        public override Room[,] GenerateRooms(int maxRooms)
+        {
+            Directions[] directions = (Directions[])Enum.GetValues(typeof(Directions));
+            List<Room> rooms = new List<Room>();
+            rooms.Add(new Room(new Point(0, 0)));
+
+            while (rooms.Count < maxRooms)
+            {
+                int index = Randomizer.GetHighRandomNumber(rooms.Count);
+                Room targetRoom = rooms[index];
+                Directions direction = Randomizer.GetRandomDirection();
+                Exit targetExit = targetRoom.GetExit(direction);
+
+                if (targetExit != null)
+                    continue;
+
+                targetExit = new Exit(direction);
+                targetRoom.AddExit(direction);
+                Room neighbor = GetRoomAtPoint(targetRoom.GetNeighborCoordinates(direction), rooms);
+                Directions neighborDirection = ReverseDirection(direction);
+
+                if (neighbor != null)
+                    neighbor.AddExit(neighborDirection);
+                else
+                {
+                    Room newRoom = new Room(targetRoom.GetNeighborCoordinates(direction));
+                    newRoom.AddExit(neighborDirection);
+                    rooms.Add(newRoom);
+                }
+            }
+            Room[,] roomArray = ConvertListToMap(MarkSpecialRooms<Room>(rooms));
+
+            return roomArray;
+        }
+    }
+}
---
+++ Momolike/TreasureRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapGen
+{
+    public class TreasureRoom : Room
+    {
+        public TreasureRoom(Point p):base(p)
+        {
+            Location = p;
+        }
+
+        public TreasureRoom(Room baseRoom):base(baseRoom.Location)
+        {
+            Location = baseRoom.Location;
+            NorthExit = baseRoom.NorthExit;
+            SouthExit = baseRoom.SouthExit;
+            EastExit = baseRoom.EastExit;
+            WestExit = baseRoom.WestExit;
+        }
+    }
+}
